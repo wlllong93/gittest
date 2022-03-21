1beb7c7051ba8c163f0b0915dcef8e666cc7f453
wll <1083750052@qq.com>
2022-03-21 09:34:27 +0800

    Initial commit

---
+++ my_use_git/src/main/java/com/people.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * ClassName:people
+ * Package:com
+ * Description:
+ *
+ * @Date:2022/3/21 9:02
+ * @Author:wll
+ */
+public class people {
+
+    private int i;
+    public double d;
+
+}
